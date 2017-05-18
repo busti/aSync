3c3b8c42b8fee7cd58ed33570ba5078d44ad083b
Busti <mbust@live.de>
2017-05-18 15:57:10 +0200

    Created svg grid.

--- flow/src/main/scala/aSync/GFlow.scala
+++ flow/src/main/scala/aSync/GFlow.scala
@@ -1,20 +1,39 @@
 package aSync
 
+import org.scalajs.dom.svg.SVG
 import org.scalajs.jquery.jQuery
 
 import scala.scalajs.js.JSApp
+import scalatags.JsDom.implicits._
+import scalatags.JsDom.svgAttrs._
+import scalatags.JsDom.svgTags._
+import scalatags.JsDom.{svgAttrs, svgTags}
 
 object GFlow extends JSApp {
   override def main() {
-    val elements = jQuery(".gFlow")
-    println(elements)
-    /*for (element <- elements) {
+    val elements = jQuery(".gFlow").get()
+    for (element <- elements) {
       element match {
         case svg: SVG =>
-          println("Initializing svg with id " + svg.id)
-        case e =>
-          println("The usage of " + e.getClass + " is not permitted.")
+          svg.appendChild(
+            defs(
+              svgTags.filter(id := "glow", x := "-5000%", y := "-5000%", width := "10000%", height := "10000%")(
+                feMorphology(in := "sourceGraphic", result := "dilated", operator := "dilate", radius := "3"),
+                feGaussianBlur(in := "dilated", result := "coloredBlur", stdDeviation := "4"),
+                feMerge(
+                  feMergeNode(in := "coloredBlur"),
+                  feMergeNode(in := "SourceGraphic")
+                )
+              ),
+              pattern(id := "grid", width := "64", height := "64", patternUnits := "userSpaceOnUse")(
+                circle(cx := "32", cy := "32", r := "1", fill := "DodgerBlue", svgAttrs.filter := "url(#glow)")
+              )
+            ).render
+          )
+          svg.appendChild(
+            rect(width := "100%", height := "100%", fill := "url(#grid)").render
+          )
       }
-    }*/
+    }
   }
 }
