4fc4fee8f9bbdbd61245259458fd336c32f4b96d
Busti <mbust@live.de>
2017-05-30 19:40:15 +0200

    Disabled web server and some testing.

--- server/src/main/scala/async/server/Server.scala
+++ server/src/main/scala/async/server/Server.scala
@@ -1,13 +1,23 @@
 package async.server
 
-import async.util.OscService
-import async.web.WebService
+import async.util.{OlaService, OscService}
 import com.typesafe.scalalogging.LazyLogging
 
 object Server extends App with LazyLogging {
   logger.info("Starting Audio-Sync")
 
-  //val ola = new OlaService
+  val ola = new OlaService
   val osc = new OscService
-  val web = new WebService
+  //val web = new WebService
+
+  var i = 0
+  while (true) {
+    i += 1
+    if (i > 255)
+      i = 0
+
+    ola.olaClient.sendDmx(1, Array.fill[Short](512)(i))
+
+    Thread.sleep(100)
+  }
 }
